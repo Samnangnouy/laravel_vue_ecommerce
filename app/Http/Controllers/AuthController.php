a55b230587eafedb94e22024ba31bd81fec4649d
Samnang John <samnangjohn@MacBook-Pro.local>
2025-06-14 22:22:24 +0700

    Display user on navbar after login

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use App\Http\Resources\UserResource;
 
 class AuthController extends Controller
 {
@@ -31,7 +32,7 @@ class AuthController extends Controller
         }
         $token = $user->createToken('main')->plainTextToken;
         return response([
-            'user' => $user,
+            'user' => new UserResource($user),
             'token' => $token
         ]);
     }
@@ -41,6 +42,10 @@ class AuthController extends Controller
         $user -> currentAccessToken()->delete();
 
         return response('', 204);
+    }
+
+    public function getUser(Request $request) {
+        return new UserResource($request->user());
     }
 
 }
